748fa28392d14a2550d5223b221bd6e78ac6b5fd
Mohammed Al-Jaff <m.aljaff90@gmail.com>
2021-02-17 18:40:50 +0100

    ...

--- lab1/bash_test_1.sh
+++ lab1/bash_test_1.sh
@@ -4,3 +4,9 @@
 
 
 samtools view /proj/g2021007/private/DATA/BAM/SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.sam | head -25
+
+echo "..............."
+echo "..............."
+
+samtools view /proj/g2021007/private/DATA/BAM/SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.sam | head -25 | cut -f1,3,5
+
