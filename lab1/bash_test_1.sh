1b57c2ceef3bc34300f6920e9d2d3a10b63a403f
Mohammed Al-Jaff <m.aljaff90@gmail.com>
2021-02-17 18:31:31 +0100

    same as last

--- lab1/bash_test_1.sh
+++ lab1/bash_test_1.sh
@@ -3,8 +3,4 @@
 #SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.bam
 # this will only print pwd
 
-echo " You be here now:"
-pwd 
-cd /proj/g2021007/private/
-echo "And now you be here:"
-pwd
+head -20 /proj/g2021007/private/DATA/BAM/SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.sam | grep SN 
