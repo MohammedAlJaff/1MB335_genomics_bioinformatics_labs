09e70f0550422bedbfd9f5f7ff315c7aa7f062d7
Mohammed Al-Jaff <m.aljaff90@gmail.com>
2021-02-17 18:23:40 +0100

    update bash test

--- lab1/bash_test_1.sh
+++ lab1/bash_test_1.sh
@@ -3,4 +3,8 @@
 #SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.bam
 # this will only print pwd
 
+echo " You be here now:"
+pwd 
+cd /proj/g2021007/private/
+echo "And now you be here:"
 pwd
