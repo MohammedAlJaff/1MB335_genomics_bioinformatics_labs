81a2f277cb2629caa49df3bb0660deaa4ab5b15d
Mohammad Al-Jaff <mohamma@rackham3.uppmax.uu.se>
2021-02-17 18:37:28 +0100

    altered bash test to include samtools

--- lab1/bash_test_1.sh
+++ lab1/bash_test_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.bam
-# this will only print pwd
 
-head -20 /proj/g2021007/private/DATA/BAM/SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.sam | grep SN 
+
+samtools view /proj/g2021007/private/DATA/BAM/SGDPDai1.dedup.realn.3mask_recal.chr2.135787850-135887184.sam | head -25
