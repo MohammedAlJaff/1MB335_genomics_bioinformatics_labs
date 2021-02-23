9b210100f284b177c012c11f349e26730e425c42
Mohammed Al-Jaff <m.aljaff90@gmail.com>
2021-02-23 13:54:04 +0100

    ...

--- lab3/assembly_copy_to_maj.sh
+++ lab3/assembly_copy_to_maj.sh
@@ -5,7 +5,7 @@
 
 
 # Copy over assembly 
-cp /proj/g2021007/private/DATA/assemblies/C_remanei/*  /proj/g2021007/private/RESULTS/mohammad_aljaff/comp_labs/1MB335_genomics_bioinformatics_labs/lab3/
+cp //proj/g2021007/private/DATA/assemblies/C_remanei/GCF_000149515.1_ASM14951v1_genomic.fna.gz /proj/g2021007/private/RESULTS/mohammad_aljaff/comp_labs/1MB335_genomics_bioinformatics_labs/lab3/
 
 # copy over set of poritens file
 cp /proj/g2021007/private/DATA/coding_sequences/*  /proj/g2021007/private/RESULTS/mohammad_aljaff/comp_labs/1MB335_genomics_bioinformatics_labs/lab3/
