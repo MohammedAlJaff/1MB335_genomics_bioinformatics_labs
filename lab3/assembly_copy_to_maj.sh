3fec7fb8ec14c40fa35a28e538eabee414536005
Mohammed Al-Jaff <m.aljaff90@gmail.com>
2021-02-23 13:49:58 +0100

    added copy assembly and proteins file scripts

---
+++ lab3/assembly_copy_to_maj.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+#The assembly is in a subfolder of: /proj/g2021007/private/DATA/assemblies
+#The set of proteins is in: /proj/g2021007/private/DATA/coding_sequences
+
+
+# Copy over assembly 
+cp /proj/g2021007/private/DATA/assemblies/*  /proj/g2021007/private/RESULTS/mohammad_aljaff/comp_labs/1MB335_genomics_bioinformatics_labs/lab3/
+
+# copy over set of poritens file
+cp /proj/g2021007/private/DATA/coding_sequences/*  /proj/g2021007/private/RESULTS/mohammad_aljaff/comp_labs/1MB335_genomics_bioinformatics_labs/lab3/
