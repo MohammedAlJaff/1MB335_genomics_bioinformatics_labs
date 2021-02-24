cf0527db546e43b245dddff517ab4ac5dc6ba14a
MohammedAlJaff <mohamma@rackham2.uppmax.uu.se>
2021-02-24 11:25:27 +0100

    chmod modifaction

--- lab3/scripts/filter_evals_from_first_tblast_results.sh
+++ lab3/scripts/filter_evals_from_first_tblast_results.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ## filter out the inital results of the tblast outfile based on a threshold e-value... in this case : 
-# 1. 0.0001
+# 1. 0.0001 = 274 hits/lines
+# 2. 0.001 = 288 hits/lines
+# 3. 0.00001 = 253 hits/lines
 
-awk '$11 < 0.0001 {print}' < tblastx_blast_outfile_for_C_elegans.blast | wc -l 
+awk '$11 < 0.00001 {print}' < tblastx_blast_outfile_for_C_elegans.blast | wc -l 
